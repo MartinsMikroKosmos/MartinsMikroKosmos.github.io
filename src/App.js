844cbcc7e52129f174efe29c1c54940e69cfb0a4
Martinus Iudex <martin.richter.1990@gmail.com>
2022-06-08 18:53:25 +0200

    styles.css Sortierdialog richtig aligned; App.js local storage geändert

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ class App extends React.Component {
   lsLoeschen() {
     if (confirm("Wollen Sie wirklich alles löschen?!")) {
       localStorage.clear()
+      window.location.reload()
     }
   }
 
